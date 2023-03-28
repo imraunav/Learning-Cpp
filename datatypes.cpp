bdcae9c17a83b4b82491eb0cb4ed9c1fcdf398ff
Raunav <raun.ghosh@gmail.com>
2023-03-28 10:30:57 +0530

    Tested datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // double pi = 3.14159;
+    float pi = 3.14159;
+    cout << "Pi = "<< pi << endl;
+    int p = int(pi);
+    cout << "Pi = "<< p << endl;
+    return 0;
+}
--- hello.cpp
+++ hello.cpp
@@ -4,4 +4,6 @@ using namespace std;
 
 int main(){
 	cout<<"Hello, world!"<<endl;
+	cout<<"Goodbye, world! ";
+	cout<<"Cruel world!"<<endl;
 }
